a93ba32c26febeb74bfe379d7b6257ba00f880d8
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-15 18:08:41 +0900

    user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,14 @@
 
 
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar")
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true)
 
 # 追加のユーザーをまとめて生成する
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name, email: email, password: password, password_confirmation: password)
-end
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name, email: email, password: password, password_confirmation: password)
+# end
+
+User.create!(name: "murapyon", email: "a@a.com", password: "111111", password_confirmation: "111111")
